51855fa1ce64187c25a061cada58aae1bc86b7c1
ankitkarki27 <ankitkarki8088@gmail.com>
2026-02-27 23:15:47 +0545

    feat(admin): remove unused admin dashboard and login view files

--- resources/views/admin/dashboard/index.blade copy.php
+++
@@ -1,9 +0,0 @@
-    @extends('admin.top')
-
-    @section('content')
-    {{-- @include('admin.layouts.app') --}}
-        <h3>Name of admin updated: {{ Auth::guard('admin')->user()->name }}</h3>
-        <h3>{{ Auth::guard('admin')->user()->email }}</h3>
-        <a href="{{ route('admin_profile') }}">profile</a>
-        <a href="{{ route('admin_logout') }}">Logout</a>
-    @endsection
